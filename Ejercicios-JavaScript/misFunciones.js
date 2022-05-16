6e6a9363141af9a3a752049d00fdfe365ae83a8b
vgaggio <2113503@ucc.edu.ar>
2022-05-16 19:32:13 -0300

    se agrego ejercicio de conversor de Unidades de js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,17 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarUnidades(id, valor){
+    if(isNaN(valor)){
+        alert("Se ingreso un valor invalido");
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value ="";
+        document.lasUnidaes.unid_pie.value = "";
+        document.lasUnidaes.unid_yarda.value = ";
+    }else if(id=="metro"){
+        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.lasUnidaes.unid_pie.value = 3.28084*valor;
+        document.lasUnidaes.unid_yarda.value = 1.09361*valor
+    }
+}
