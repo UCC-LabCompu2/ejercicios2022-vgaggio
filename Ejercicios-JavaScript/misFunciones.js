6f776c7b1a26739e7e88c97c7b060700f29f3f36
vgaggio <2113503@ucc.edu.ar>
2022-05-16 20:48:53 -0300

    Se modifico actividad de operaciones matematica segun como seguia el ejercicio

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -54,5 +54,5 @@ function calcularSuma(){
 
     num1=Number(document.getElementByName("sum_num1")[0].value);
     num2=document.getElementByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value=num1 + Number(num2);
+    document.getElementsByName("sum_total")[0].innerHTML=num1 + Number(num2);
 }
