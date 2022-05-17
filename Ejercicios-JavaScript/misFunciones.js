b46a80258d4dab2453269c8f25c98b821991118b
vgaggio <2113503@ucc.edu.ar>
2022-05-17 00:03:09 -0300

    Se agrego ejercicio de canvas cuadriculado

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -127,3 +127,54 @@ function limpiarCanvas(){
 
 
 }
+
+function dibujarCuadriculado(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var alturaMax = canvas.height;
+    var anchoMax = canvas.width;
+
+    //dibujar lineas horizontales
+    ctx.beginPath();
+    for(var i=0; i<alturaMax;){
+        ctx.moveTo(0,i);
+        ctx.lineTo(anchoMax,i);
+        ctx.strokeStyle = "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+    //dibujar lineas verticales
+    ctx.beginPath();
+    for(var i=0; i<anchoMaxx;){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,alturaMax);
+        ctx.strokeStyle = "#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+
+    //Eje X
+    ctx.beginPath();
+    ctx.moveTo(0,alturaMax/2);
+    ctx.lineTo(anchoMax,alturaMax/2);
+    ctx.strokeStyle = "#d91c00";
+    ctx.stroke();
+    i=i+20;
+    ctx.closePath();
+
+    //Eje Y
+    ctx.beginPath();
+    ctx.moveTo(anchoMax/2,0);
+    ctx.lineTo(anchoMax/2,alturaMax);
+    ctx.strokeStyle = "#d91c00";
+    ctx.stroke();
+    i=i+20;
+    ctx.closePath();
+
+
+
+}
