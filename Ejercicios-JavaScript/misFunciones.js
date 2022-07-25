d439f11a2899830372313e643b49604cc2a88aad
vgaggio <2113503@ucc.edu.ar>
2022-07-25 12:59:08 -0300

    Se agrego ejercicios de operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -65,7 +65,31 @@ function calcularSuma(){
     num2=document.getElementByName("sum_num2")[0].value;
     document.getElementsByName("sum_total")[0].innerHTML=num1 + Number(num2);
 }
-document.lasUnidades.unid_metro.value = Math.round(metro*100)/100;
+function calcularResta(){
+    var num1, num2;
+
+    num1=Number(document.getElementByName("res_num1")[0].value);
+    num2=document.getElementByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].innerHTML=num1 + Number(num2);
+}
+
+function calcularMul(){
+    var num1, num2;
+
+    num1=Number(document.getElementByName("mul_num1")[0].value);
+    num2=document.getElementByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].innerHTML=num1 + Number(num2);
+}
+
+function calcularDiv(){
+    var num1, num2;
+
+    num1=Number(document.getElementByName("div_num1")[0].value);
+    num2=document.getElementByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].innerHTML=num1 + Number(num2);
+}
+
+
 function cargarWeb(){
 
     var cant, unidad, urlComp;
